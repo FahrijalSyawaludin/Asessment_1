6d9bc6fa9c1ffc6d2414f480e84634f3b7fd1def
FahrijalSyawaludin <fahrijals@student.telkomuniversity.ac.id>
2023-03-26 21:06:51 +0700

    Initial Commit

---
+++ app/src/main/java/com/fahrijal/pemesanantiket/database/AppDatabase.kt
@@ -0,0 +1,20 @@
+package com.fahrijal.pemesanantiket.database
+
+import androidx.room.Database
+import com.fahrijal.pemesanantiket.model.ModelDatabase
+import androidx.room.RoomDatabase
+import com.fahrijal.pemesanantiket.database.dao.DatabaseDao
+
+/**
+ * Created by Azhar Rivaldi on 19-11-2021
+ * Youtube Channel : https://bit.ly/2PJMowZ
+ * Github : https://github.com/AzharRivaldi
+ * Twitter : https://twitter.com/azharrvldi_
+ * Instagram : https://www.instagram.com/azhardvls_
+ * LinkedIn : https://www.linkedin.com/in/azhar-rivaldi
+ */
+
+@Database(entities = [ModelDatabase::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun databaseDao(): DatabaseDao?
+}
